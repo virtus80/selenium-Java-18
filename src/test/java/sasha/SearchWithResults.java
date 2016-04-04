e2b0065164e53bd4bbff186dc854fa58c16e4e51
virtus80 <virtus80@mail.ru>
2016-04-04 21:55:38 +0300

    HomeTask4 - changes for remarks
    - added waitings of search for search with results;
    - added 'target' and 'test-output' folders for gitignore

--- src/test/java/sasha/SearchWithResults.java
+++ src/test/java/sasha/SearchWithResults.java
@@ -8,6 +8,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchWithResults extends TestBase {
   private boolean acceptNextAlert = true;
@@ -16,20 +18,19 @@ public class SearchWithResults extends TestBase {
   @Test
   public void testSearchWithResults() throws Exception {    
 	driver.get(baseUrl + "/php4dvd/");
+	WebDriverWait wait = new WebDriverWait(driver, 20);
+	WebElement filmBeforeSearch = driver.findElement(By.cssSelector("div.movie_box"));
     driver.findElement(By.id("q")).clear();
     String searchString = "star";
     driver.findElement(By.id("q")).sendKeys(searchString + Keys.RETURN);
-    List <WebElement> elements = driver.findElements(By.cssSelector("div.movie_box"));
-    for (int second = 0;; second++) {
-    	if (second >= 60) fail("timeout");
-    	try { 
-    			for(WebElement film : elements) {
-    				String title = film.findElement(By.cssSelector("div.title")).getText();
-    				assertThat(title, containsString(searchString));	
-    			}  
-    		} catch (Exception e) {}
-    	Thread.sleep(1000);
+    
+    wait.until(ExpectedConditions.stalenessOf(filmBeforeSearch));
+    try{
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.movie_box"))); 
+    } catch (TimeoutException ex) {
+        
     }
+   
 
    }
 }
